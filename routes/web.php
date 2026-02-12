b51bd5c3dd2ea65bdc314eeaeb78a75f084d1d28
thadeu <thadeu@thadeu.com>
2026-02-12 14:53:34 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+Route::any('{path?}', function ($path = '') {
+    $targetUrl = 'https://notifications.zig.tickets/' . $path;
+
+    if (request()->getQueryString()) {
+        $targetUrl .= '?' . request()->getQueryString();
+    }
+
+    $response = Http::withHeaders(
+        collect(request()->headers->all())
+            ->except(['host', 'content-length'])
+            ->map(fn ($values) => $values[0])
+            ->toArray()
+    )->send(
+        request()->method(),
+        $targetUrl,
+        [
+            'body' => request()->getContent(),
+        ]
+    );
+
+    return response($response->body(), $response->status())
+        ->withHeaders($response->headers());
+})->where('path', '.*');
